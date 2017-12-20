b4de7e878428da9b1f9151390a594d8287bcf7b3
PC-20170217VTRI\Administrator <304146788@qq.com>
2017-12-20 12:50:16 +0800

    修改评论资源类型

--- szwhg-core/src/main/java/com/creatoo/szwhg/core/model/ResourceType.java
+++ szwhg-core/src/main/java/com/creatoo/szwhg/core/model/ResourceType.java
@@ -7,7 +7,7 @@ import io.swagger.annotations.ApiModel;
  */
 @ApiModel(value = "资源类型")
 public enum ResourceType {
-    Activity("活动"),Train("培训"),Information("资讯"),ArtTeam("文化团队"),ArtWorks("征集作品"),
+    Activity("活动"),Train("培训"),Information("资讯"),ArtTeam("文化团队"),ArtWorks("征集作品"),ArtTalent("文艺人才"),DigitalShow("数字展览"),CultureSupply("文化配送"),
     heritageDirectory("非遗名录"),heritageSuccessor("传承人"),Venue("场馆"),VolunteerRecruit("招募活动"),LiveVideos("直播"), Demands ("录播");
     private String desc;
 
